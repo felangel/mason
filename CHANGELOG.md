418525c962ab33788089831b7707801e6155046d
Felix Angelov <felangelov@gmail.com>
2020-11-17 01:26:56 -0600

    docs: inline documentation updates

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+# 0.0.1-dev.2
+
+- docs: inline documentation updates
+
 # 0.0.1-dev.1
 
 **Dev Release**
--- lib/mason.dart
+++ lib/mason.dart
@@ -1,1 +1,11 @@
+/// A Dart template generator which helps teams
+/// generate files quickly and consistently.
+///
+/// ```sh
+/// # activate mason
+/// pub global activate mason
+///
+/// # see usage
+/// mason --help
+/// ```
 library mason;
