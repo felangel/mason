95e004acef60c898cf9eb0d6a1ff26e478c9f949
Felix Angelov <felangelov@gmail.com>
2021-02-15 19:52:29 -0600

    fix test

--- test/fixtures/bundle/dart/greeting_bundle.dart
+++ test/fixtures/bundle/dart/greeting_bundle.dart
@@ -2,15 +2,4 @@
 
 import 'package:mason/mason.dart';
 
-final greetingBundle = MasonBundle.fromJson({
-  "files": [
-    {
-      "path": "../bricks/greeting/__brick__/GREETINGS.md",
-      "data": "SGkge3tuYW1lfX0hCg==",
-      "type": "text"
-    }
-  ],
-  "name": "greeting",
-  "description": "A Simple Greeting Template",
-  "vars": ["name"]
-});
+final greetingBundle = MasonBundle.fromJson({"files":[{"path":"../bricks/greeting/__brick__/GREETINGS.md","data":"SGkge3tuYW1lfX0hCg==","type":"text"}],"name":"greeting","description":"A Simple Greeting Template","vars":["name"]});
