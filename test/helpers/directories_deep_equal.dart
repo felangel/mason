b54317f1db85f860d663c2cceebe227e6ee692cf
Felix Angelov <felangelov@gmail.com>
2021-05-23 22:31:49 -0500

    test: more debugging

--- test/helpers/directories_deep_equal.dart
+++ test/helpers/directories_deep_equal.dart
@@ -23,9 +23,9 @@ bool directoriesDeepEqual(Directory? a, Directory? b) {
 
     if (path.basename(fileA.path) != path.basename(fileB.path)) return false;
     if (!_equality.equals(fileA.readAsBytesSync(), fileB.readAsBytesSync())) {
-      print(fileA.readAsBytesSync());
+      print(fileA.readAsStringSync());
       print('---');
-      print(fileB.readAsBytesSync());
+      print(fileB.readAsStringSync());
       return false;
     }
   }
