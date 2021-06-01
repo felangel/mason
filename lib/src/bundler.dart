626b468d6a519858db70e073e97647fabda3327c
Felix Angelov <felangelov@gmail.com>
2021-06-01 13:08:30 -0500

    update test case

--- lib/src/bundler.dart
+++ lib/src/bundler.dart
@@ -35,9 +35,7 @@ Future<MasonBundle> createBundle(Directory brick) async {
 MasonBundledFile _bundleFile(File file) {
   final fileType =
       _binaryFileTypes.hasMatch(path.basename(file.path)) ? 'binary' : 'text';
-  final data = base64.encode(fileType == 'text'
-      ? utf8.encode(file.readAsStringSync().replaceAll(r'\r', ''))
-      : file.readAsBytesSync());
+  final data = base64.encode(file.readAsBytesSync());
   final filePath = path.joinAll(
     path.split(file.path).skipWhile((value) => value != BrickYaml.dir).skip(1),
   );
