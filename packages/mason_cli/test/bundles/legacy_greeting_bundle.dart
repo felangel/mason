78e521301547ba68fb1f9845586fbbf87b8b5293
Felix Angelov <felangelov@gmail.com>
2022-01-16 13:12:45 -0600

    revert: legacy greeting bundle

---
+++ packages/mason_cli/test/bundles/legacy_greeting_bundle.dart
@@ -0,0 +1,14 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: prefer_single_quotes, public_member_api_docs, lines_longer_than_80_chars, implicit_dynamic_list_literal, implicit_dynamic_map_literal
+
+import 'package:mason/mason.dart';
+
+final legacyGreetingBundle = MasonBundle.fromJson(<String, dynamic>{
+  "files": [
+    {"path": "GREETINGS.md", "data": "SGkge3tuYW1lfX0h", "type": "text"}
+  ],
+  "name": "greeting",
+  "description": "A Simple Greeting Template",
+  "version": "1.0.0",
+  "vars": ['name']
+});
