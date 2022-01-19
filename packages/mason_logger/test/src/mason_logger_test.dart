c7b66bd1dc8acb8a2c77e39d74fb13f2072512f5
Felix Angelov <felangelov@gmail.com>
2022-01-18 22:58:53 -0600

    fix(mason_logger): progress/update string truncation

--- packages/mason_logger/test/src/mason_logger_test.dart
+++ packages/mason_logger/test/src/mason_logger_test.dart
@@ -326,7 +326,7 @@ void main() {
             verify(
               () {
                 stdout.write(
-                  '''${lightGreen.wrap('\b${'\b' * (message.length + 4)}✓')} $message ${darkGray.wrap('(0.1s)')}\n''',
+                  '''\b${'\b' * (message.length + 4)}\u001b[2K${lightGreen.wrap('✓')} $message ${darkGray.wrap('(0.1s)')}\n''',
                 );
               },
             ).called(1);
