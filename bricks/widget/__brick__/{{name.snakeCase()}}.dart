fdda271702e00afebfba8336d5310bffe2092a9c
Sandro Lovnički <lovnicki.sandro@gmail.com>
2022-04-19 19:34:56 +0200

    bricks/widget/: [fix] usage of child

--- bricks/widget/__brick__/{{name.snakeCase()}}.dart
+++ bricks/widget/__brick__/{{name.snakeCase()}}.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 
 class {{name.pascalCase()}} extends StatelessWidget {
-  const {{name.pascalCase()}}({Key key}) : super(key: key);
+  const {{name.pascalCase()}}({
+    Key? key,
+    required this.child,
+  }) : super(key: key);
+
+  final Widget child;
 
   @override
   Widget build(BuildContext context) {
