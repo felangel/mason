1ee6066e8c2350e8b1d66dd244a7278542aa0895
Elijah Luckey <luckeyelijah112@gmail.com>
2022-06-01 09:38:52 -0400

    Adds `search` usage to test

--- packages/mason_cli/test/src/command_runner_test.dart
+++ packages/mason_cli/test/src/command_runner_test.dart
@@ -35,6 +35,7 @@ const expectedUsage = [
       '  new        Creates a new brick template.\n'
       '  publish    Publish the current brick to brickhub.dev.\n'
       '  remove     Removes a brick.\n'
+      '  search     Search registered bricks on brickhub.dev.\n'
       '  unbundle   Generates a brick template from a bundle.\n'
       '  update     Update mason.\n'
       '  upgrade    Upgrade bricks to their latest versions.\n'
