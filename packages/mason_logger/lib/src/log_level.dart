23109caf43830863bba4638663774c6f09cbd905
Jochum van der Ploeg <jochum.vanderploeg@verygood.ventures>
2022-07-12 22:51:09 +0200

    feat: support log levels

--- packages/mason_logger/lib/src/log_level.dart
+++ packages/mason_logger/lib/src/log_level.dart
@@ -1,10 +1,28 @@
+/// Indicates what level of logging is desired.
 enum LogLevel {
+  /// Log detail, success, info, warning, error, and alert messages.
   detail,
+
+  /// Log success, info, warning, error, and alert messages.
   info,
-  error,
-  warn,
-  alert,
+
+  /// Log success, info, warning, error, and alert messages.
   success,
+
+  /// Log warning, error and alert messages.
+  warn,
+
+  /// Log error and alert messages.
+  error,
+
+  /// Only log alert messages
+  alert,
+
+  /// Logs everything, no matter what levels will be added in the future.
+  ///
+  /// This is the default level.
   all,
+
+  /// No logging.
   none,
 }
