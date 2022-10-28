080ea86fae5ecd28760d3f96927d087b134975e8
Felix Angelov <felangelov@gmail.com>
2022-10-27 23:04:34 -0500

    fix(mason_logger): only animate progress on terminals

---
+++ packages/mason_logger/example/test.dart
@@ -0,0 +1,10 @@
+import 'package:mason_logger/mason_logger.dart';
+
+Future<void> main() async {
+  final logger = Logger();
+  final progress = logger.progress('Calculating');
+  await Future<void>.delayed(const Duration(seconds: 1));
+  progress.update('Almost done');
+  await Future<void>.delayed(const Duration(seconds: 1));
+  progress.complete('Done');
+}
