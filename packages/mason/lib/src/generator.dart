b67864175d08a3e1073f8888f23244911086b3d1
Felix Angelov <felangelov@gmail.com>
2022-11-17 00:22:28 -0600

    use .mason-cache/bundled directory

--- packages/mason/lib/src/generator.dart
+++ packages/mason/lib/src/generator.dart
@@ -59,8 +59,10 @@ class MasonGenerator extends Generator {
   /// Factory which creates a [MasonGenerator] based on
   /// a local [MasonBundle].
   static Future<MasonGenerator> fromBundle(MasonBundle bundle) async {
-    final target = Directory.systemTemp.createTempSync();
-    unpackBundle(bundle, target);
+    final target = Directory(
+      p.join(BricksJson.bundled.path, '${bundle.name}_${bundle.version}'),
+    );
+    if (!target.existsSync()) unpackBundle(bundle, target);
     return MasonGenerator._fromBrick(target.path);
   }
 
