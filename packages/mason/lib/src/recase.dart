565355147d5a3809015149374c3f93b303ab7305
LukeMoody01 <luke.moody@xam-consulting.com>
2023-03-27 19:26:12 +1100

    chore: Fix analysis

--- packages/mason/lib/src/recase.dart
+++ packages/mason/lib/src/recase.dart
@@ -113,6 +113,6 @@ class ReCase {
   }
 
   String _upperCaseFirstLetter(String word) {
-    return '${word.substring(0, 1).toUpperCase()}${word.substring(1).toLowerCase()}';
+    return '''${word.substring(0, 1).toUpperCase()}${word.substring(1).toLowerCase()}''';
   }
 }
