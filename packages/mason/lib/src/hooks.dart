01a05791ed3644d23f22d1e193b4d4aab11cb0f3
Felix Angelov <felangelov@gmail.com>
2024-05-28 20:17:42 -0500

    chore: revert logging

--- packages/mason/lib/src/hooks.dart
+++ packages/mason/lib/src/hooks.dart
@@ -253,9 +253,7 @@ class GeneratorHooks {
     if (uri == null) throw HookMissingRunException(hook.path);
 
     const mode = _isAotCompiled ? 'aot-snapshot' : 'kernel';
-    final progress = logger?.progress(
-      'Compiling ${p.basename(hook.path)} ($mode)',
-    );
+    final progress = logger?.progress('Compiling ${p.basename(hook.path)}');
     final result = await Process.run(
       'dart',
       ['compile', mode, uri.toFilePath()],
