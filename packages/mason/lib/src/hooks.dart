1893ea696f6cc5d489424ed651cab537c777ed7a
Felix Angelov <felangelov@gmail.com>
2024-05-28 20:03:10 -0500

    fix(mason): compile hooks to AOT when using AOT runtime

--- packages/mason/lib/src/hooks.dart
+++ packages/mason/lib/src/hooks.dart
@@ -251,10 +251,14 @@ class GeneratorHooks {
     final uri = await _getHookUri(hook, checksum);
 
     if (uri == null) throw HookMissingRunException(hook.path);
-    final progress = logger?.progress('Compiling ${p.basename(hook.path)}');
+
+    const mode = _isAotCompiled ? 'aot-snapshot' : 'kernel';
+    final progress = logger?.progress(
+      'Compiling ${p.basename(hook.path)} ($mode)',
+    );
     final result = await Process.run(
       'dart',
-      ['compile', 'kernel', uri.toFilePath()],
+      ['compile', mode, uri.toFilePath()],
       runInShell: true,
     );
 
