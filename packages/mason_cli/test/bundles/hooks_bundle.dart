139eb91f4cd4baa65a32c128b674044e7cafb35e
Felix Angelov <felangelov@gmail.com>
2024-08-06 11:45:51 -0500

    Update packages/mason_cli/test/bundles/hooks_bundle.dart

--- packages/mason_cli/test/bundles/hooks_bundle.dart
+++ packages/mason_cli/test/bundles/hooks_bundle.dart
@@ -5,7 +5,6 @@ import 'package:mason/mason.dart';
 
 final hooksBundle = MasonBundle.fromJson(<String, dynamic>{
   "files": [
-    // cspell:disable-next-line
     {"path": "hooks.md", "data": "SGkge3tuYW1lfX0h", "type": "text"}
   ],
   "hooks": [
