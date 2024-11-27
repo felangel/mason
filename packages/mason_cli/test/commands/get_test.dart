aeaf476056407de5ce899196fa7540d4e58d2f10
Felix Angelov <felangelov@gmail.com>
2024-11-27 14:13:52 -0600

    fix: failing tests

--- packages/mason_cli/test/commands/get_test.dart
+++ packages/mason_cli/test/commands/get_test.dart
@@ -337,7 +337,7 @@ bricks:
       final brickYaml = File(path.join('example', 'brick.yaml'));
       brickYaml.writeAsStringSync(
         brickYaml.readAsStringSync().replaceFirst(
-              'mason: ">=${mason.packageVersion} <0.1.0"',
+              'mason: ^${mason.packageVersion}',
               'mason: ">=99.99.99 <100.0.0"',
             ),
       );
