9b67e5429f94cdf1757b3425b19f638ca773598e
Felix Angelov <felangelov@gmail.com>
2026-09-15 12:52:57 -0500

    fix: mason app icon test

--- packages/mason_cli/test/commands/make_test.dart
+++ packages/mason_cli/test/commands/make_test.dart
@@ -558,7 +558,7 @@ bricks:
         'make',
         'app_icon',
         '--url',
-        'https://storage.googleapis.com/cms-storage-bucket/c823e53b3a1a7b0d36a9.png',
+        'https://flutter.dev/assets/lockup_flutter_horizontal.d0515092173211776ceed19b39c2a041.png',
       ]);
       expect(result, equals(ExitCode.success.code));
 
